7d1ae0cd4b03df67791e951cdef3d188f7219a0c
Kanha Tomar <kanharajput91@gmail.com>
2022-02-18 10:38:53 +0530

    set onClickListener over items

--- app/src/main/java/com/example/learnrecyclerview/WordListAdapter.java
+++ app/src/main/java/com/example/learnrecyclerview/WordListAdapter.java
@@ -26,7 +26,7 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     @Override   // this method is called to create a view
     public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = mInflater.inflate(R.layout.wordlist,parent,false);
-        return new WordViewHolder(itemView);
+        return new WordViewHolder(itemView,this);
     }
 
     @Override   // used to bind data with the view referenced in the WordViewHolder constructor
@@ -41,11 +41,27 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     }
 
     // referencing the view
-    public class WordViewHolder extends RecyclerView.ViewHolder {
+    public class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView wordView;
-        public WordViewHolder(@NonNull View itemView) {
+        WordListAdapter mWordListAdapter;
+        public WordViewHolder(@NonNull View itemView,WordListAdapter mwordListAdapter) {
             super(itemView);
             wordView = itemView.findViewById(R.id.word);
+            this.mWordListAdapter = mwordListAdapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // get the position of the item that was clicked
+            int mPosition = getLayoutPosition();
+            // use this position to get the affected item in the wordlist
+            String element = mwordList.get(mPosition);
+            // change the data in the wordList
+            mwordList.set(mPosition,"cicked!"+element);
+            // notify the adapter that the data has been chagned
+            // so recyclerVeiw can update it
+            mWordListAdapter.notifyDataSetChanged();
         }
     }
 }
